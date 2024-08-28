e2e922b5d6edf506179e425a10d23bafdd30c09e
SNSantos <stephane.nascimentosantos@heig-vd.ch>
2024-08-28 11:35:18 +0200

    Authentification buttons

--- frontend/src/client/Home/HomePage.jsx
+++ frontend/src/client/Home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import SubmitForm from '../../components/SubmitForm';  
+import SubmitForm from '../../components/SubmitForm';
+import AuthButtons from '../../components/AuthButtons';  
 import { useClient } from '../hooks/useClient';
 import { useNavigate } from 'react-router-dom';
 import './home.css'
@@ -20,23 +21,42 @@ function Home() {
         navigate('/billing_overview');
     };
 
+    const handleLogin = () => {
+  
+        // Add your log in logic here, like showing a log in form or redirecting
+      };
+    
+      // Function to handle the Sign Up button click
+    const handleSignUp = () => {
+     
+        // Add your sign up logic here, like showing a sign up form or redirecting
+      };
+
     return (
         <div>
+            <AuthButtons
+            buttonLogInText="Connexion"
+            buttonSignUpText="Inscription"
+            onLogin={handleLogin}
+            onSignUp={handleSignUp}
+            />
             <h1>Welcome to Our Website</h1>
             <p>This is the homepage where you can find an overview of our services and features.</p>
+           
             <SubmitForm
                 label="Numéro de plaque : "
-                placeholder="VDO9815..."
+                placeholder="VD09815..."
                 buttonText="Valider"
                 onSubmit={handleSubmit}
             />
-
-            <nav>
+             <nav>
                 <ul>
                     <li><Link to="/billing_overview">Billing Overview</Link></li>
                     <li><Link to="/admin">Admin Section</Link></li>
                 </ul>
             </nav>
+         
+
         </div>
     );
 }
